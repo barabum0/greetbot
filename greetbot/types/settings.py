aafeeae7b017c5888583c2eb82819410478ec68e
roman mac <barabum@duck.com>
2024-02-15 16:35:26 +0300

    🔧 feat: добавил поддержку MongoDB в настройках и переменные окружения

--- greetbot/types/settings.py
+++ greetbot/types/settings.py
@@ -1,9 +1,14 @@
+from typing import Annotated
+
+from pydantic import MongoDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(extra="ignore", env_prefix="greetbot__", env_nested_delimiter="__")
 
+    mongo_url: Annotated[str, MongoDsn]
+    mongo_db_name: str
     bot_token: str
 
 
