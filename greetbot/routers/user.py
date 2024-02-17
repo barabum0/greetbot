b58e1caf8530770c4809634b86c3643d1a52485b
roman mac <barabum@duck.com>
2024-02-17 14:36:11 +0300

    ✨ feat(routers): добавил создание и подтверждение пользователя

--- greetbot/routers/user.py
+++ greetbot/routers/user.py
@@ -1,10 +1,15 @@
 from aiogram import Router
 from aiogram.types import ChatJoinRequest
-from loguru import logger
+
+from greetbot.types.user import ChannelUser
 
 router = Router(name="User")
 
 
 @router.chat_join_request()
 async def chat_join(request: ChatJoinRequest):
-    logger.info("User {user} requested", user=request.from_user.username)
+    db_user = await ChannelUser.get(request.from_user.id)
+    if not db_user:
+        db_user = await ChannelUser(id=request.from_user.id).insert()
+
+    await request.approve()
