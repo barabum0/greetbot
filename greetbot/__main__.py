688d52baa32e5f56e984875e6bede3ef8b21be6c
roman mac <barabum@duck.com>
2024-02-17 13:20:32 +0300

    ✨ feat(main): добавил основной модуль бота

---
+++ greetbot/__main__.py
@@ -0,0 +1,17 @@
+from aiogram import Dispatcher, Bot
+
+from greetbot.routers import init_routers
+from greetbot.services.database import init_database
+from greetbot.services.logs import configure_logger
+from greetbot.types.settings import settings
+
+
+async def main() -> None:
+    dispatcher = Dispatcher()
+    bot = Bot(token=settings.bot_token, parse_mode="HTML")
+
+    configure_logger()
+    await init_database()
+    init_routers(dispatcher)
+
+    await dispatcher.start_polling(bot)
