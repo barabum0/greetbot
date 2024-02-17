f1245ac0e7082b23bf07f3d54ee6680d83a41880
roman mac <barabum@duck.com>
2024-02-17 13:20:18 +0300

    ✨ feat(router): добавил обработку запросов на вступление в чат

---
+++ greetbot/routers/user.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+from aiogram.types import ChatJoinRequest
+from loguru import logger
+
+router = Router(name="User")
+
+
+@router.chat_join_request
+async def chat_join(request: ChatJoinRequest):
+    logger.info("User {user} requested", user=request.from_user.username)
