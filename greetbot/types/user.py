dd373309147453e2a246e3235461c0a95779bfcc
roman mac <barabum@duck.com>
2024-02-17 13:10:15 +0300

    🛠 fix(types): удалил лишнее поле channel_id из ChannelUser

--- greetbot/types/user.py
+++ greetbot/types/user.py
@@ -6,7 +6,6 @@ from pydantic import Field
 
 class ChannelUser(Document):
     user_id: int
-    channel_id: int
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
