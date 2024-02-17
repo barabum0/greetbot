2a6a017a4e249b199fa46df2c1b234d9b12b3fed
roman mac <barabum@duck.com>
2024-02-17 13:17:44 +0300

    ✨ feat(logs): добавил класс InterceptHandler и настройку логгирования

--- greetbot/services/logs.py
+++ greetbot/services/logs.py
@@ -1,6 +1,13 @@
 import sys
 
+import logging
 from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        level = logger.level(record.levelname).name
+        logger.log(level, record.getMessage())
 
 
 def configure_logger() -> None:
@@ -23,3 +30,8 @@ def configure_logger() -> None:
     logger.level("WARNING", color="<yellow>")
     logger.level("ERROR", color="<red>")
     logger.level("CRITICAL", color="<bold><white><RED>")
+
+    logging.getLogger('aiogram').setLevel(logging.DEBUG)
+    logging.getLogger('aiogram').addHandler(InterceptHandler())
+    logging.getLogger('asyncio').setLevel(logging.DEBUG)
+    logging.getLogger('asyncio').addHandler(InterceptHandler())
