9fe04c63d4ac69f4324f0bd5f1f57355fc11237f
roman mac <barabum@duck.com>
2024-02-19 20:01:12 +0300

    🔧 fix(routers): добавил admin роутер в инициализацию

--- greetbot/routers/__init__.py
+++ greetbot/routers/__init__.py
@@ -1,11 +1,12 @@
 from aiogram import Dispatcher
 
-from greetbot.routers import user
+from greetbot.routers import user, admin
 
 
 def init_routers(dp: Dispatcher) -> None:
     routers = [
-        user.router
+        user.router,
+        admin.router
     ]
 
     dp.include_routers(*routers)
