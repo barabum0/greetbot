d11c4ebfc224f606dbd66a2cf8a02fc2a6a70085
roman mac <barabum@duck.com>
2024-02-19 20:01:05 +0300

    ✨ feat(routers): добавил отправку приветствий ботом

--- greetbot/routers/user.py
+++ greetbot/routers/user.py
@@ -1,15 +1,23 @@
-from aiogram import Router
+from aiogram import Router, Bot
 from aiogram.types import ChatJoinRequest
 
+from greetbot.types.bot_config import Greeting
 from greetbot.types.user import User
 
 router = Router(name="User")
 
 
 @router.chat_join_request()
-async def chat_join(request: ChatJoinRequest):
+async def chat_join(request: ChatJoinRequest, bot: Bot):
     db_user = await User.get(request.from_user.id)
     if not db_user:
         db_user = await User(id=request.from_user.id).insert()
 
     await request.approve()
+
+    greetings = await Greeting.find(Greeting.is_enabled == True).to_list()
+    for greeting in greetings:
+        try:
+            await greeting.send_as_aiogram_message(bot, request.from_user.id)
+        except:
+            pass
