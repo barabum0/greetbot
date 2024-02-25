113c833ca9cffc88a215365b81713f1ed48cc79a
roman mac <barabum@duck.com>
2024-02-25 16:57:45 +0300

    ✨ feat(types): добавил поддержку идентификации пользователей по каналу

--- greetbot/types/user.py
+++ greetbot/types/user.py
@@ -1,14 +1,24 @@
 from datetime import datetime
+from typing import Self
 
 from beanie import Document
-from pydantic import Field
+from pydantic import Field, BaseModel
+
+
+class ChannelUserID(BaseModel):
+    user_id: int
+    channel_id: int
 
 
 class User(Document):
-    id: int  # type: ignore
+    id: ChannelUserID  # type: ignore
     is_blocked: bool = False
     is_admin: bool = False
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
         name = "users"
+
+    @classmethod
+    async def get_by_id(cls, user_id: int, channel_id) -> Self | None:
+        return await cls.get(ChannelUserID(user_id=user_id, channel_id=channel_id))
