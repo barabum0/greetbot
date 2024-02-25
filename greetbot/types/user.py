a2cae09356adfb87747a94333a1fdbf60d0cb34e
roman mac <barabum@duck.com>
2024-02-25 16:58:20 +0300

    Revert "✨ feat(types): добавил поддержку идентификации пользователей по каналу"
    This reverts commit 113c833ca9cffc88a215365b81713f1ed48cc79a.

--- greetbot/types/user.py
+++ greetbot/types/user.py
@@ -1,24 +1,14 @@
 from datetime import datetime
-from typing import Self
 
 from beanie import Document
-from pydantic import Field, BaseModel
-
-
-class ChannelUserID(BaseModel):
-    user_id: int
-    channel_id: int
+from pydantic import Field
 
 
 class User(Document):
-    id: ChannelUserID  # type: ignore
+    id: int  # type: ignore
     is_blocked: bool = False
     is_admin: bool = False
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
         name = "users"
-
-    @classmethod
-    async def get_by_id(cls, user_id: int, channel_id) -> Self | None:
-        return await cls.get(ChannelUserID(user_id=user_id, channel_id=channel_id))
