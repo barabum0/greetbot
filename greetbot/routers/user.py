efdae3eab4311e3ebd1e77a1b5a2069f3b66b005
roman mac <barabum@duck.com>
2024-03-11 20:32:43 +0300

    ✨ feat: добавил поддержку подтверждения вступления в чат

--- greetbot/routers/user.py
+++ greetbot/routers/user.py
@@ -1,17 +1,25 @@
-from aiogram import Router, Bot
-from aiogram.types import ChatJoinRequest
+from aiogram import Router, Bot, F
+from aiogram.fsm.context import FSMContext
+from aiogram.types import ChatJoinRequest, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 from greetbot.types.bot_config import Greeting
+from greetbot.types.settings import settings
 from greetbot.types.user import User
 
 router = Router(name="User")
 
 
 @router.chat_join_request()
-async def chat_join(request: ChatJoinRequest, bot: Bot):
+async def chat_join(request: ChatJoinRequest, bot: Bot, state: FSMContext):
     db_user = await User.get(request.from_user.id)
     if not db_user:
         db_user = await User(id=request.from_user.id).insert()
+
+    if settings.require_request_confirmation:
+        await bot.send_message(request.from_user.id, settings.request_confirmation_message_text.replace("{{ channel_name }}", request.chat.full_name), reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[[InlineKeyboardButton(text=settings.request_confirmation_button_text,
+                                                   callback_data=f"accept_request_{request.chat.id}")]]))
+        return
 
     await request.approve()
 
@@ -20,5 +28,22 @@ async def chat_join(request: ChatJoinRequest, bot: Bot):
     for greeting in greetings:
         try:
             await greeting.send_as_aiogram_message(bot, request.from_user.id, request.from_user)
+        except:
+            pass
+
+
+@router.callback_query(F.data.startswith("accept_request_"))
+async def accept_request(call: CallbackQuery, bot: Bot, state: FSMContext):
+    *_, chat_id = call.data.split("_")  # type: ignore
+
+    # TODO: сделать более простой метод + защиту от ошибок
+    greetings = await Greeting.find(Greeting.is_enabled == True).to_list()
+
+    await bot.approve_chat_join_request(chat_id, call.from_user.id)
+    await call.message.delete()  # type: ignore
+
+    for greeting in greetings:
+        try:
+            await greeting.send_as_aiogram_message(bot, call.from_user.id, call.from_user)
         except:
             pass
